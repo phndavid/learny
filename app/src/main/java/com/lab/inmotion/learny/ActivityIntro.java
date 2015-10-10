1f0827c4cf8ad7ff4b88d45c2f0d69f76e0709e0
phndavid13 <phndavid@gmail.com>
2015-10-10 18:05:20 -0500

    Init app

--- app/src/main/java/com/lab/inmotion/learny/ActivityIntro.java
+++ app/src/main/java/com/lab/inmotion/learny/ActivityIntro.java
@@ -1,5 +1,6 @@
 package com.lab.inmotion.learny;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,42 +12,16 @@ import android.view.MenuItem;
 
 public class ActivityIntro extends AppCompatActivity {
 
+    public void btnLogin(View view){
+        Intent theIntent = new Intent(this, ActivityLogin.class);
+        startActivity(theIntent);
+        finish();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_intro);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_intro, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/lab/inmotion/learny/ActivityLogin.java
+++ app/src/main/java/com/lab/inmotion/learny/ActivityLogin.java
@@ -1,5 +1,6 @@
 package com.lab.inmotion.learny;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,21 +10,15 @@ import android.view.View;
 
 public class ActivityLogin extends AppCompatActivity {
 
+    public void btnSignIn(View view){
+        Intent theIntent = new Intent(this, ActivityPlay.class);
+        startActivity(theIntent);
+        finish();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
