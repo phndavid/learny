aa69c5ae5d07b52f53e213603e3fd11411f2cacb
phndavid13 <phndavid@gmail.com>
2015-11-21 00:19:28 -0500

    form paciente

--- app/src/main/java/com/lab/inmotion/learny/Activities/SequenceActivity.java
+++ app/src/main/java/com/lab/inmotion/learny/Activities/SequenceActivity.java
@@ -1,31 +1,85 @@
 package com.lab.inmotion.learny.Activities;
 
+import android.content.ClipData;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.lab.inmotion.learny.R;
 
 public class SequenceActivity extends AppCompatActivity {
 
+    private int[] imgResourceIds = {R.mipmap.plantauno,R.mipmap.plantados,R.mipmap.plantatres};
+    private int[] fontResourceIds = {R.mipmap.fondouno,R.mipmap.fondodos,R.mipmap.fondotres,R.mipmap.fondocuatro,R.mipmap.fondocinco};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sequence);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        findViewById(R.id.imgOne).setOnTouchListener(new imgTouchListener());
+        findViewById(R.id.imgTwo).setOnTouchListener(new imgTouchListener());
+        findViewById(R.id.imgThree).setOnTouchListener(new imgTouchListener());
+        findViewById(R.id.linearOne).setOnDragListener(new ContainerDragListener());
+        findViewById(R.id.linearTwo).setOnDragListener(new ContainerDragListener());
+        findViewById(R.id.linearThree).setOnDragListener(new ContainerDragListener());
+
+    }
+    private class imgTouchListener implements View.OnTouchListener {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+
+    	/*ACTION_DOWN -> A pressed gesture has started, the motion contains the initial position*/
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+        /*Creates an image that the system displays during the drag and drop operation. This is called a "drag shadow".*/
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDrag(data, shadowBuilder, view, 0);
+        /*while the img is moving, the view is invisible, if we comment this line, the view 'will move' when we drop it*/
+                view.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
             }
-        });
+        }
+    }
+
+    private class ContainerDragListener implements View.OnDragListener {
+        //Drawable enterShape = getResources().getDrawable(R.drawable.shape_droptarget);
+        //Drawable normalShape = getResources().getDrawable(R.drawable.shape);
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int action = event.getAction();
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    // do nothing
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+    	             /*change de background*/
+                    // v.setBackground(enterShape);
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    //v.setBackground(normalShape);
+                    break;
+                case DragEvent.ACTION_DROP:
+                    // Dropped, reassign View to ViewGroup
+                    View view = (View) event.getLocalState();
+                    LinearLayout oldContainer = (LinearLayout) view.getParent();
+                    oldContainer.removeView(view);
+                    LinearLayout newContainer = (LinearLayout) v;
+                    newContainer.addView(view);
+                    view.setVisibility(View.VISIBLE);
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    //v.setBackground(normalShape);
+                default:
+                    break;
+            }
+            return true;
+        }
     }
 
 }
