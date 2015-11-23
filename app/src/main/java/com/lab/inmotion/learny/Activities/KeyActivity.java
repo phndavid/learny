ecb15e01cb48fece4149416f8a1acebdbfdde566
JuanCamiloMendozaBalanta <camilomendoza_95@hotmail.com>
2015-11-22 20:13:23 -0500

    Merge branch 'master' of C:\Users\CAMILO15\AndroidStudioProjects\Learny with conflicts.

--- app/src/main/java/com/lab/inmotion/learny/Activities/KeyActivity.java
+++ app/src/main/java/com/lab/inmotion/learny/Activities/KeyActivity.java
@@ -1,23 +1,52 @@
 package com.lab.inmotion.learny.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.lab.inmotion.learny.Application.App;
+import com.lab.inmotion.learny.Model.Learny;
 import com.lab.inmotion.learny.R;
+
+import java.lang.reflect.Field;
+
+import static com.lab.inmotion.learny.R.*;
 
 public class KeyActivity extends AppCompatActivity {
 
-    private int[] imgResourceIds = {R.mipmap.uno, R.mipmap.dos,R.mipmap.tres,R.mipmap.cuatro,R.mipmap.cinco,R.mipmap.seis,R.mipmap.siete,
-            R.mipmap.ocho,R.mipmap.nueve};
-    private String[] numbers = {"1234","3124","7653","9842","7812"};
-    public void btnOne(View view ){
+    private RelativeLayout relativeLayout;
+    private ImageView imgFigure;
+    private TextView textNumber;
+    private String word;
+    private LinearLayout miL;
+    private Learny model;
+    private ImageView elIma;
 
+    private  int count;
+    private int[] imgResourceIds = {R.mipmap.uno, R.mipmap.dos, R.mipmap.tres, R.mipmap.cuatro, R.mipmap.cinco, R.mipmap.seis, R.mipmap.siete,
+            R.mipmap.ocho, R.mipmap.nueve};
+    private String[] numbers = {"31242","76532","98421","78128"};
+
+    public void btnOne(View view ){
+        ImageView myImage = new ImageView(this);
+        myImage.setImageResource(R.mipmap.uno);
+        miL.addView(myImage);
+        setContentView(miL);
     }
     public void btnTwo(View view ){
+        ImageView myImage = new ImageView(this);
+        myImage.setImageResource(R.mipmap.uno);
+        miL.addView(myImage);
+        setContentView(miL);
 
     }
     public void btnThree(View view ){
@@ -42,13 +71,54 @@ public class KeyActivity extends AppCompatActivity {
 
     }
     public void btnContinueKey(View view){
+        Intent intent = new Intent(this,FeedBackActivity.class);
+        intent.putExtra("category", "descifraClave");
+        intent.putExtra("count", model.getCurrent().getCurrentTest().getId());
+        startActivity(intent);
+        count++;
+        int actual = model.getCurrent().getCurrentTest().getId();
+        if(actual<4) {
+            System.out.println("puntaje acumulado:" + model.getCurrent().getPuntaje());
+            model.nextTest();
+        }
 
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_key);
-
+        setContentView(layout.activity_key);
+        setContentView(layout.activity_figure);
+        System.out.println("se ejecuta el oncreate");
+        imgFigure = (ImageView) findViewById(id.imgFigure);
+        textNumber = (TextView) findViewById(id.textViewNumber);
+        App app = (App) getApplication();
+        model = app.getModel();
+        miL= (LinearLayout) findViewById(id.linearLa);
     }
 
+    protected void onResume() {
+        super.onResume();
+        System.out.println("OnResume Counter Sequence: " +count);
+        int thecount= model.getCurrent().getCurrentTest().getId();
+        if(thecount>0) {
+            switch (thecount){
+                case 1:
+                    word = word+numbers[1];
+                    textNumber.setText(word);
+                    break;
+                case 2:
+                    word = word+numbers[2];
+                    textNumber.setText(word);
+                    break;
+                case 3:
+                    word = word+numbers[3];
+                    textNumber.setText(word);
+                    break;
+                case 4:
+                    word = word+numbers[4];
+                    textNumber.setText(word);
+                    break;
+            }
+        }
+    }
 }
