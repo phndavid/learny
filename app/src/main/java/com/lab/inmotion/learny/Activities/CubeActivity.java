1cb5a407e87502970e56f772d2a5a077dbbb847e
phndavid13 <phndavid@gmail.com>
2015-11-24 02:43:23 -0500

    Validacion Cubos check

--- app/src/main/java/com/lab/inmotion/learny/Activities/CubeActivity.java
+++ app/src/main/java/com/lab/inmotion/learny/Activities/CubeActivity.java
@@ -1,23 +1,15 @@
 package com.lab.inmotion.learny.Activities;
 
 import android.content.ClipData;
-import android.content.ClipDescription;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Layout;
-import android.util.Log;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
-
 import com.lab.inmotion.learny.Application.App;
 import com.lab.inmotion.learny.Model.Learny;
 import com.lab.inmotion.learny.R;
@@ -30,89 +22,77 @@ public class CubeActivity extends AppCompatActivity {
     private int[] fontResourceIds = {R.mipmap.figurauno,R.mipmap.figurados,R.mipmap.figuratres,
             R.mipmap.figuracuatro,R.mipmap.figuracinco};
 
+    private int[] resourceIds = {R.mipmap.pqdos,R.mipmap.pqdos,R.mipmap.pquno, R.mipmap.pquno,
+            R.mipmap.pquno,R.mipmap.pqdos,R.mipmap.pqdos,R.mipmap.pquno,
+            R.mipmap.pqdos,R.mipmap.pqtres,R.mipmap.pqdos,R.mipmap.pqseis,
+            R.mipmap.pqcinco,R.mipmap.pqseis,R.mipmap.pquno,R.mipmap.pquno,
+            R.mipmap.pqseis,R.mipmap.pqcinco,R.mipmap.pqtres,R.mipmap.pqcuatro,
+    };
+
     private ImageView imgCubeOne;
     private ImageView imgCubeTwo;
     private ImageView imgCubeThree;
     private ImageView imgCubeFour;
     private ImageView imgMuestra;
 
+    public LinearLayout linearOne;
+    public LinearLayout linearTwo;
+    public LinearLayout linearThree;
+    public LinearLayout linearFour;
+
     private Learny model;
 
-    //Para puntaje--------------------------
-    private LinearLayout elLayoutUno;
-    private LinearLayout elLayoDos;
-    private LinearLayout elLayoTres;
-    private LinearLayout elLayoCuatro;
-
-    private View viewUno;
-    private View viewDos;
-    private View viewTres;
-    private View viewCuatro;
-    //-------------------------------
-
+    public void checkAnswer(int count){
+        ImageView img1 = (ImageView) linearOne.getChildAt(0);
+        ImageView img2 = (ImageView) linearTwo.getChildAt(0);
+        ImageView img3 = (ImageView) linearThree.getChildAt(0);
+        ImageView img4 = (ImageView) linearFour.getChildAt(0);
+        if(img1 != null && img2 != null && img3 != null && img4 != null) {
+            if (img1.getBackground().getConstantState() == getResources().getDrawable(resourceIds[count*4]).getConstantState()) {
+                if (img2.getBackground().getConstantState() == getResources().getDrawable(resourceIds[count*4+1]).getConstantState())
+                    if (img3.getBackground().getConstantState() == getResources().getDrawable(resourceIds[count*4+2]).getConstantState())
+                        if (img4.getBackground().getConstantState() == getResources().getDrawable(resourceIds[count*4+3]).getConstantState()){
+                            Toast.makeText(this, "Figura correcta!", Toast.LENGTH_LONG).show();
+                        }
+            }
+        }
+    }
     public void btnContinueCube(View view){
         Intent intent = new Intent(this,FeedBackActivity.class);
         intent.putExtra("category","organizalos");
         intent.putExtra("count", model.getCurrent().getCurrentTest().getId());
         startActivity(intent);
         finish();
-        getPositionCount();
         int actual = model.getCurrent().getCurrentTest().getId();
         if(actual<4) {
             System.out.println("puntaje acumulado: " + model.getCurrent().getPuntaje());
             model.nextTest();
+            checkAnswer(actual);
         }
-    }
-    public  void getPositionCount(){
-        int[] locations1 = new int[2];
-        imgCubeOne.getLocationOnScreen(locations1);
-        int x1 = locations1[0];
-        int y1 = locations1[1];
-        int[] locations2 = new int[2];
-        imgCubeTwo.getLocationOnScreen(locations2);
-        int x2 = locations2[0];
-        int y2 = locations2[1];
-        int[] locations3 = new int[2];
-        imgCubeThree.getLocationOnScreen(locations3);
-        int x3 = locations3[0];
-        int y3 = locations3[1];
-        int[] locations4 = new int[2];
-        imgCubeFour.getLocationOnScreen(locations4);
-        int x4 = locations4[0];
-        int y4 = locations4[1];
-
-        String msg = "IMG1 X:"+x1+" Y:"+y1+" - IMG2 X:"+x2+" Y:"+y2+" - IMG3 X:"+x3+" Y:"+y3+" - IMG4 X:"+x4+" Y:"+y4;
-        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
-
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cube);
-
         imgMuestra = (ImageView) findViewById(R.id.img_figureCube);
         imgCubeOne = (ImageView) findViewById(R.id.cubeOne);
         imgCubeTwo = (ImageView) findViewById(R.id.cubeTwo);
         imgCubeThree = (ImageView) findViewById(R.id.cubeThree);
         imgCubeFour = (ImageView) findViewById(R.id.cubeFour);
-
-        //para contar Nuevo
-        View view = (View) findViewById(R.id.buttonStop);
-        elLayoutUno= (LinearLayout) view;
-        //
-
-        //termina
-
         findViewById(R.id.cubeOne).setOnTouchListener(new imgTouchListener());
         findViewById(R.id.cubeTwo).setOnTouchListener(new imgTouchListener());
         findViewById(R.id.cubeThree).setOnTouchListener(new imgTouchListener());
         findViewById(R.id.cubeFour).setOnTouchListener(new imgTouchListener());
         findViewById(R.id.right_img).setOnDragListener(new ContainerDragListener());
-        findViewById(R.id.right_imgCuatro).setOnDragListener(new ContainerDragListener());
         findViewById(R.id.right_imgDos).setOnDragListener(new ContainerDragListener());
         findViewById(R.id.right_imgTres).setOnDragListener(new ContainerDragListener());
+        findViewById(R.id.right_imgCuatro).setOnDragListener(new ContainerDragListener());
         findViewById(R.id.left_img).setOnDragListener(new ContainerDragListener());
+        linearOne = (LinearLayout) findViewById(R.id.right_img);
+        linearTwo = (LinearLayout) findViewById(R.id.right_imgDos);
+        linearThree = (LinearLayout) findViewById(R.id.right_imgTres);
+        linearFour = (LinearLayout) findViewById(R.id.right_imgCuatro);
         App app = (App) getApplication();
         model = app.getModel();
     }
@@ -183,6 +163,7 @@ public class CubeActivity extends AppCompatActivity {
                     break;
                 case 2:
                     imgMuestra.setBackgroundResource(fontResourceIds[2]);
+
                     imgCubeOne.setBackgroundResource(imgResourceIds[2]);
                     imgCubeTwo.setBackgroundResource(imgResourceIds[5]);
                     imgCubeThree.setBackgroundResource(imgResourceIds[1]);
@@ -190,10 +171,11 @@ public class CubeActivity extends AppCompatActivity {
                     break;
                 case 3:
                     imgMuestra.setBackgroundResource(fontResourceIds[3]);
+
                     imgCubeOne.setBackgroundResource(imgResourceIds[0]);
                     imgCubeTwo.setBackgroundResource(imgResourceIds[4]);
                     imgCubeFour.setBackgroundResource(imgResourceIds[0]);
-                    imgCubeThree.setBackgroundResource(imgResourceIds[3]);
+                    imgCubeThree.setBackgroundResource(imgResourceIds[5]);
                     break;
                 case 4:
                     imgMuestra.setBackgroundResource(fontResourceIds[4]);
@@ -201,7 +183,7 @@ public class CubeActivity extends AppCompatActivity {
                     imgCubeOne.setBackgroundResource(imgResourceIds[3]);
                     imgCubeTwo.setBackgroundResource(imgResourceIds[4]);
                     imgCubeFour.setBackgroundResource(imgResourceIds[2]);
-                    imgCubeThree.setBackgroundResource(imgResourceIds[0]);
+                    imgCubeThree.setBackgroundResource(imgResourceIds[5]);
                     break;
                 case 5:
 
